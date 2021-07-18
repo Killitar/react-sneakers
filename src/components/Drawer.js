903853c8670eac030c78377be75e7428e66f30a7
Miroslav <lincovsci02@gmail.com>
2021-07-18 15:44:08 +0300

    Создал React компоненты

---
+++ src/components/Drawer.js
@@ -0,0 +1,39 @@
+function Drawer() {
+  return (
+    <div hidden className="overlay">
+      <div className="drawer d-flex flex-column p-30">
+        <h2 className="mb-30 d-flex justify-between">
+          Корзина <img className="removeBtn cu-p" src="img/btn-remove.svg" alt="Remove" />
+        </h2>
+
+        <div className="items flex">
+          <div className="cartItem d-flex align-center p-20 mb-20">
+            <img className="mr-20" width={70} height={70} src="sneakers/1.jpg" alt="Sneakers" />
+            <div className="mr-20">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="removeBtn" src="img/btn-remove.svg" alt="Remove" />
+          </div>
+        </div>
+        <ul className="cartTotalBlock">
+          <li>
+            <span>Итого:</span>
+            <div></div>
+            <b>21 498 руб. </b>
+          </li>
+          <li>
+            <span>Налог 5%: </span>
+            <div></div>
+            <b>1074 руб. </b>
+          </li>
+        </ul>
+        <button className="greenButton">
+          Оформить заказ <img src="img/arrow.svg" alt="Arrow" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
