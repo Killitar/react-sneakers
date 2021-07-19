589739d6e2f1a66486ace1ac36f5c3a7e34d682d
Miroslav <lincovsci02@gmail.com>
2021-07-19 12:35:52 +0300

    использовал css modules, сделал карточки товаров с помощью React props.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const Arr = [
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: 'sneakers/1.jpg' },
+  { name: 'Мужские Кроссовки Nike Air Max 270', price: 12999, imageUrl: 'sneakers/2.jpg' },
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: 'sneakers/3.jpg' },
+  { name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8499, imageUrl: 'sneakers/4.jpg' },
+  { name: 'Мужские Кроссовки Under Armour Curry 8', price: 15199, imageUrl: 'sneakers/5.jpg' },
+  { name: 'Мужские Кроссовки Nike Kyrie 7', price: 11299, imageUrl: 'sneakers/6.jpg' },
+  { name: 'Мужские Кроссовки Jordan Air Jordan 11', price: 10799, imageUrl: 'sneakers/7.jpg' },
+  { name: 'Мужские Кроссовки Nike LeBron XVIII', price: 16499, imageUrl: 'sneakers/8.jpg' },
+  { name: 'Мужские Кроссовки Nike Lebron XVIII Low', price: 13999, imageUrl: 'sneakers/9.jpg' },
+  { name: 'Мужские Кроссовки Nike Kyrie Flytrap IV', price: 8499, imageUrl: 'sneakers/10.jpg' },
+  { name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: 'sneakers/4.jpg' },
+  { name: 'Мужские Кроссовки Nike Kyrie 7', price: 11999, imageUrl: 'sneakers/6.jpg' },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -15,8 +30,10 @@ function App() {
             <input placeholder="Поиск..." />
           </div>
         </div>
-        <div className="d-flex">
-          <Card />
+        <div className="d-flex flex-wrap ">
+          {Arr.map((obj) => (
+            <Card title={obj.name} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,17 @@
-function Card() {
+import styles from './Card.module.scss';
+
+function Card(props) {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="img/heart-unliked.svg" alt="heart-unliked" />
       </div>
-      <img width={133} height={112} src="sneakers/1.jpg" alt="sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span className="opacity-5 text-uppercase">Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
 
         <button className="button">
